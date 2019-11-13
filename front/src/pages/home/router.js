88923d436f41f987d32c48fd93307836531efe1f
jhon sebastian valencia <sebasjsv97@gmail.com>
2019-11-13 15:34:29 -0500

    versionamiento de back/.env

--- front/src/pages/home/router.js
+++ front/src/pages/home/router.js
@@ -1,73 +1,67 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router';
-import Axios from 'axios';
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Axios from "axios";
 
 Vue.prototype.$http = Axios;
 Vue.use(VueRouter);
 
-import PDocumentBuilder from '@/pages/home/documentBuilder/PDocumentBuilder.vue';
-import PDocumentBuilderRoutes from '@/pages/home/documentBuilder/router';
-import PStats from '@/pages/home/stats/PStats.vue';
+import PDocumentBuilder from "@/pages/home/documentBuilder/PDocumentBuilder.vue";
+import PDocumentBuilderRoutes from "@/pages/home/documentBuilder/router";
+import PStats from "@/pages/home/stats/PStats.vue";
 
 const routes = [
-    {
-        path: '/stats',
-        component: PStats
-    },
-    {
-        path: '/document',
-        component: PDocumentBuilder,
-        children: PDocumentBuilderRoutes
-    },
-    {
-        path: '',
-        redirect: '/stats'
-    }
+  {
+    path: "/stats",
+    component: PStats
+  },
+  {
+    path: "/document",
+    component: PDocumentBuilder,
+    children: PDocumentBuilderRoutes
+  },
+  {
+    path: "",
+    redirect: "/stats"
+  }
 ];
 
 const router = new VueRouter({
-    routes
+  routes
 });
 
 router.beforeEach((to, from, next) => {
-    if (!router.app.$session.exists()) {
-        let key = localStorage.getItem('key');
-        let token = localStorage.getItem('token');
+  if (!router.app.$session.exists()) {
+    let key = localStorage.getItem("key");
+    let token = localStorage.getItem("token");
 
-        if (!key || !token) {
-            throw 'Debe iniciar sesión';
-        }
-
-        Axios.request({
-            url: `${process.env.VUE_APP_MODULE_API_ROUTE}auth/checkSaiaToken`,
-            method: 'post',
-            responseType: 'json',
-            data: {
-                key,
-                token
-            }
-        })
-            .then(response => {
-                router.app.$session.start();
-                router.app.$session.set('key', key);
-                router.app.$session.set(
-                    'externalToken',
-                    response.data.externalToken
-                );
-                router.app.$session.set(
-                    'apiToken',
-                    'Bearer ' + response.data.apiToken
-                );
-
-                next();
-            })
-            .catch(response => {
-                console.error(response.message);
-                next(false);
-            });
-    } else {
-        next();
+    if (!key || !token) {
+      throw "Debe iniciar sesión";
     }
+
+    Axios.request({
+      url: `${process.env.VUE_APP_MODULE_API_ROUTE}auth/checkSaiaToken`,
+      method: "post",
+      responseType: "json",
+      data: {
+        key,
+        token
+      }
+    })
+      .then(response => {
+        router.app.$session.start();
+        router.app.$session.set("key", key);
+        router.app.$session.set("externalToken", response.data.externalToken);
+        router.app.$session.set("apiToken", "Bearer " + response.data.apiToken);
+
+        next();
+      })
+      .catch(response => {
+        console.error(response.message);
+        next(false);
+      });
+  } else {
+    next();
+  }
 });
 
 export { router as default };
